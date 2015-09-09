9c310da240b3e5e2c3636fea3d7825acad7201b1
Yuan Ji <jiwhiz@gmail.com>
2015-09-08 20:36:03 -0600

    read ocr output as UTF-8

--- server-ocr/src/main/java/com/openprice/ocr/TesseractCommandImpl.java
+++ server-ocr/src/main/java/com/openprice/ocr/TesseractCommandImpl.java
@@ -2,6 +2,7 @@ package com.openprice.ocr;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,7 +21,7 @@ public class TesseractCommandImpl implements Tesseract {
             final Process p = Runtime.getRuntime().exec(command);
             p.waitFor();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream(), StandardCharsets.UTF_8));
             String line;
             while ((line = reader.readLine())!= null) {
                 output.append(line + "\n");
