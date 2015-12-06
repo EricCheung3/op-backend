ed094561f72bced2619027d40f2757d253058ec1
Yuan Ji <jiwhiz@gmail.com>
2015-12-05 17:11:48 -0700

    fix bug

--- server-admin-api/src/main/java/com/openprice/AdminApiApplicationSettings.java
+++ server-admin-api/src/main/java/com/openprice/AdminApiApplicationSettings.java
@@ -1,6 +1,5 @@
 package com.openprice;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import com.openprice.common.client.ServiceSettings;
@@ -8,7 +7,6 @@ import com.openprice.common.client.ServiceSettings;
 import lombok.Getter;
 import lombok.Setter;
 
-@SpringBootApplication
 @ConfigurationProperties("application.settings")
 public class AdminApiApplicationSettings extends AbstractApplicationSettings {
 
