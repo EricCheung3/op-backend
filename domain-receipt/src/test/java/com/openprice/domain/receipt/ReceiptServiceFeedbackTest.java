aa9766981f60cfab2a585bb4823edcfe5540542a
Yuan Ji <jiwhiz@gmail.com>
2016-01-17 15:41:16 -0700

    add skeleton code for #3

---
+++ domain-receipt/src/test/java/com/openprice/domain/receipt/ReceiptServiceFeedbackTest.java
@@ -0,0 +1,11 @@
+package com.openprice.domain.receipt;
+
+import org.junit.Test;
+
+public class ReceiptServiceFeedbackTest {
+
+    @Test
+    public void addFeedback_ShouldCreateNewFeedbackAndChangeNeedFeedback() throws Exception {
+        // TODO add new feedback and check receipt.needFeedback changed to false
+    }
+}
