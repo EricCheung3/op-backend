9bb7cbb5ff0e9bc3cdf8cb5cf7372e35d3b21e88
Tiger <tigzhanghit@gmail.com>
2016-01-22 08:39:13 -0700

    issue#9

--- domain-receipt/src/test/java/com/openprice/domain/receipt/ReceiptServiceTest.java
+++ domain-receipt/src/test/java/com/openprice/domain/receipt/ReceiptServiceTest.java
@@ -91,7 +91,12 @@ public class ReceiptServiceTest {
         final StoreBranch branch = StoreBranch.builder().branchName("Calgary Trail").build();
         final Map<ReceiptField, ValueLine> fieldToValueLine = new HashMap<ReceiptField, ValueLine>();
         fieldToValueLine.put(ReceiptField.Total, ValueLine.builder().line(-1).value("15.00").build());
-        final ParsedReceipt receiptDebug = ParsedReceipt.builder().chain(chain).branch(branch).fieldToValueMap(fieldToValueLine).items(items).build();
+        final ParsedReceipt receiptDebug = ParsedReceipt.builder()
+                                                        .chain(chain)
+                                                        .branch(branch)
+                                                        .fieldToValueMap(fieldToValueLine)
+                                                        .items(items)
+                                                        .build();
 
         when(receiptResultRepositoryMock.findFirstByReceiptOrderByCreatedTimeDesc(eq(receipt))).thenReturn(null);
         when(receiptImageRepositoryMock.findByReceiptOrderByCreatedTime(eq(receipt))).thenReturn(images);
@@ -122,18 +127,6 @@ public class ReceiptServiceTest {
                                        .user(testUser)
                                        .build();
         
-        final ReceiptImage image1 = new ReceiptImage();
-        image1.setReceipt(receipt);
-        image1.setFileName("test1");
-        image1.setOcrResult("ocr result1");
-        final ReceiptImage image2 = new ReceiptImage();
-        image2.setReceipt(receipt);
-        image2.setFileName("test2");
-        image2.setOcrResult("ocr result2");
-        final List<ReceiptImage> images = Arrays.asList(image1, image2);
-        
-        final List<String> ocrTextList = Arrays.asList("ocr result1", "ocr result2");
-        
         final List<Item> items = new ArrayList<>();
         items.add(new Item("milk", "10.99", "1.99", "4.00", "food"));
         items.add(new Item("eggs", "4.99", "4.99", "12", "food"));
@@ -142,19 +135,16 @@ public class ReceiptServiceTest {
         final StoreBranch branch = StoreBranch.builder().branchName("Calgary Trail").build();
         final Map<ReceiptField, ValueLine> fieldToValueLine = new HashMap<ReceiptField, ValueLine>();
         fieldToValueLine.put(ReceiptField.Total, ValueLine.builder().line(-1).value("15.00").build());
-        final ParsedReceipt parsedreceipt = ParsedReceipt.builder().chain(chain).branch(branch).fieldToValueMap(fieldToValueLine).items(items).build();
+        final ParsedReceipt parsedReceipt = ParsedReceipt.builder()
+                                                         .chain(chain)
+                                                         .branch(branch)
+                                                         .fieldToValueMap(fieldToValueLine)
+                                                         .items(items)
+                                                         .build();
         
-        final ReceiptResult receiptResult = receipt.createReceiptResultFromParserResult(parsedreceipt);
+        final ReceiptResult receiptResult = receipt.createReceiptResultFromParserResult(parsedReceipt);
         
         when(receiptResultRepositoryMock.findFirstByReceiptOrderByCreatedTimeDesc(eq(receipt))).thenReturn(receiptResult);
-        when(receiptImageRepositoryMock.findByReceiptOrderByCreatedTime(eq(receipt))).thenReturn(images);
-        when(simpleParser.parseOCRResults(eq(ocrTextList))).thenReturn(parsedreceipt);
-        when(receiptResultRepositoryMock.save(isA(ReceiptResult.class))).thenAnswer(new Answer<ReceiptResult>() {
-            @Override
-            public ReceiptResult answer(InvocationOnMock invocation) throws Throwable {
-                return (ReceiptResult) invocation.getArguments()[0];
-            }
-        });
         
         final ReceiptResult result = serviceToTest.getLatestReceiptResult(receipt);
         assertEquals("rcss", result.getChainCode());
@@ -162,7 +152,6 @@ public class ReceiptServiceTest {
         verify(receiptResultRepositoryMock, times(0)).save(isA(ReceiptResult.class));
     }
     
-    // test if image has no ocr result.
     @Test
     public void getLatestReceiptParserResult_ShouldReturnNull_IfReceiptImageOCRIsEmpty() throws Exception {
         final UserAccount testUser = UserAccount.testObjectBuilder()
@@ -189,15 +178,10 @@ public class ReceiptServiceTest {
         when(receiptResultRepositoryMock.findFirstByReceiptOrderByCreatedTimeDesc(eq(testReceipt))).thenReturn(null);
         when(receiptImageRepositoryMock.findByReceiptOrderByCreatedTime(eq(testReceipt))).thenReturn(images);
         when(simpleParser.parseOCRResults(eq(ocrTextList))).thenReturn(null);
-        when(receiptResultRepositoryMock.save(isA(ReceiptResult.class))).thenAnswer(new Answer<ReceiptResult>() {
-            @Override
-            public ReceiptResult answer(InvocationOnMock invocation) throws Throwable {
-                return (ReceiptResult) invocation.getArguments()[0];
-            }
-        });
         
         final ReceiptResult result = serviceToTest.getLatestReceiptResult(testReceipt);
         assertNull(result);
+        verify(receiptImageRepositoryMock, times(0)).save(isA(ReceiptImage.class));
         verify(receiptResultRepositoryMock, times(0)).save(isA(ReceiptResult.class));
     }
     
