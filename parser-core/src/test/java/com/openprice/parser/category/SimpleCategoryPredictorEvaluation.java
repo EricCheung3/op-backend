7023d83af85a01b2b40f6a8abf4a73571bc7b8a5
hengshuai yao <hengshuai@gmail.com>
2016-02-02 14:31:36 -0700

    adding SimpleCategoryPredictorEvaluation

---
+++ parser-core/src/test/java/com/openprice/parser/category/SimpleCategoryPredictorEvaluation.java
@@ -0,0 +1,9 @@
+package com.openprice.parser.category;
+
+/**
+ *  an evaluation of SimpleCategoryPredictor
+ *  It evaluates by using RCSS and Safeway's edited producCategory.txt
+ */
+public class SimpleCategoryPredictorEvaluation {
+
+}
