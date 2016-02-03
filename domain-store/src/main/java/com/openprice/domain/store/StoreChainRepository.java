0787f9f8ef424d1bc88dfc080b02148ed659d26a
Yuan Ji <jiwhiz@gmail.com>
2016-02-03 16:50:03 -0700

    delete domain-store

--- domain-store/src/main/java/com/openprice/domain/store/StoreChainRepository.java
+++
@@ -1,8 +0,0 @@
-package com.openprice.domain.store;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreChainRepository extends JpaRepository<StoreChain, String> {
-
-    StoreChain findByCode(String code);
-}
