49545e3e4c4864e4cf413de5e4a1af2bd2b53908
Yuan Ji <jiwhiz@gmail.com>
2016-02-02 20:55:23 -0700

    add branch data

---
+++ parser-api/src/main/java/com/openprice/store/StoreChainData.java
@@ -0,0 +1,24 @@
+package com.openprice.store;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class StoreChainData {
+
+    private String code;
+
+    private String name;
+
+    private String identity;
+
+    private String category;
+}
