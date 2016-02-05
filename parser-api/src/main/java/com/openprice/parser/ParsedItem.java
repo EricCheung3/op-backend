4dba026eae59eb1ead005f7252e99de089991d8d
Yuan Ji <jiwhiz@gmail.com>
2016-02-05 10:53:52 -0700

    finish parser-api design for #55

---
+++ parser-api/src/main/java/com/openprice/parser/ParsedItem.java
@@ -0,0 +1,37 @@
+package com.openprice.parser;
+
+/**
+ * Value of receipt item from ReceiptParser result
+ */
+public interface ParsedItem {
+
+    /**
+     * Product item name the parser found out. Must not be empty.
+     *
+     * @return
+     */
+    String getParsedName();
+
+    /**
+     * Product item buy price the parser found out.
+     *
+     * @return null if parser cannot find item price.
+     */
+    String getParsedBuyPrice();
+
+    /**
+     * If parser can find matching catalog, return the code.
+     *
+     * @return null if no catalog product can be found in store chain catalog list, or no matching chain for this receipt.
+     */
+    String getCatalogCode();
+
+    /**
+     * Line number for this item in the original receipt OCR result text.
+     * If multiple lines, return the first line number.
+     *
+     * @return
+     */
+    int getLineNumber();
+
+}
--- parser-api/src/main/java/com/openprice/parser/ReceiptParser.java
+++ parser-api/src/main/java/com/openprice/parser/ReceiptParser.java
@@ -2,7 +2,16 @@ package com.openprice.parser;
 
 import java.util.List;
 
+/**
+ * Interface for receipt parser.
+ */
 public interface ReceiptParser {
 
+    /**
+     * Parses OCR result text and return ParsedReceipt object.
+     *
+     * @param ocrTextList list of strings from OCR result text.
+     * @return null if ocrTextList is null or it has less than 5 lines of string.
+     */
     ParsedReceipt parseReceiptOcrResult(List<String> ocrTextList);
 }
