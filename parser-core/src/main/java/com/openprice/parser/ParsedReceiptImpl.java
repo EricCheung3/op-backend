add98a965a81bece8784a46cf9d0a6d7e16abe8f
hengshuai yao <hengshuai@gmail.com>
2016-02-05 12:59:33 -0700

    ParsedReceiptImpl fixed

--- parser-core/src/main/java/com/openprice/parser/ParsedReceiptImpl.java
+++ parser-core/src/main/java/com/openprice/parser/ParsedReceiptImpl.java
@@ -4,29 +4,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.openprice.parser.data.Item;
 import com.openprice.parser.data.ParsedFieldImpl;
 import com.openprice.parser.data.ValueLine;
 
 public class ParsedReceiptImpl implements ParsedReceipt{
     final StoreChain chain;
-    final List<Item> items;
+    final List<ParsedItem> items;
     final Map<ReceiptFieldType, ValueLine> fieldToValueMap;
 
     StoreBranch branch=StoreBranch.EmptyStoreBranch();
 
-    private ParsedReceiptImpl(final StoreChain chain, final List<Item> items, final  Map<ReceiptFieldType, ValueLine> map){
+    private ParsedReceiptImpl(final StoreChain chain, final List<ParsedItem> items, final  Map<ReceiptFieldType, ValueLine> map){
         this.chain=chain;
         this.items=items;
         this.fieldToValueMap=map;
     }
 
-    private ParsedReceiptImpl(final StoreChain chain, final List<Item> items, final  Map<ReceiptFieldType, ValueLine> map, final StoreBranch branch){
+    private ParsedReceiptImpl(final StoreChain chain, final List<ParsedItem> items, final  Map<ReceiptFieldType, ValueLine> map, final StoreBranch branch){
         this(chain, items, map);
         this.branch=branch;
     }
 
-    public static ParsedReceiptImpl fromChainItemsMapBranch(final StoreChain chain, final List<Item> items, final  Map<ReceiptFieldType, ValueLine> map, final StoreBranch branch){
+    public static ParsedReceiptImpl fromChainItemsMapBranch(final StoreChain chain, final List<ParsedItem> items, final  Map<ReceiptFieldType, ValueLine> map, final StoreBranch branch){
         return new ParsedReceiptImpl(chain, items, map, branch);
     }
 
@@ -50,7 +49,6 @@ public class ParsedReceiptImpl implements ParsedReceipt{
 
     @Override
     public List<ParsedItem> getItems() {
-        // TODO Auto-generated method stub
-        return null;
+        return items;
     }
 }
--- parser-core/src/main/java/com/openprice/parser/data/ParsedItemImpl.java
+++ parser-core/src/main/java/com/openprice/parser/data/ParsedItemImpl.java
@@ -18,4 +18,6 @@ public class ParsedItemImpl implements ParsedItem{
         this.catalogCode=code;
         this.lineNumber=line;
     }
+
+
 }
