cde5b65e54b297257af2017da5b55ec7a6550916
hengshuai yao <hengshuai@gmail.com>
2016-02-24 10:19:05 -0700

    parsers

---
+++ parser-stores/src/main/java/com/openprice/parser/store/ikea/IkeaSelector.java
@@ -0,0 +1,42 @@
+package com.openprice.parser.store.ikea;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.openprice.parser.ChainRegistry;
+import com.openprice.parser.StoreConfigImpl;
+import com.openprice.parser.api.ReceiptData;
+import com.openprice.parser.api.StoreParser;
+import com.openprice.parser.store.AbstractStoreParserSelector;
+import com.openprice.store.StoreMetadata;
+
+@Service
+public class IkeaSelector extends AbstractStoreParserSelector {
+    private Ikea1 parser;
+
+    private static final String CODE="EdoJapan";
+
+    @Inject
+    public IkeaSelector(final ChainRegistry chainRegistry, final StoreMetadata metadata) {
+        super(chainRegistry, metadata);
+    }
+
+    @Override
+    public StoreParser selectParser(final ReceiptData receipt) {
+        // just one parser now. TODO based on receipt data to return parser
+        return parser;
+    }
+
+    @Override
+    protected String getParserBaseCode() {
+        return CODE;
+    }
+
+    @Override
+    protected void generateParser() {
+        StoreConfigImpl config = loadParserConfig(CODE+"1");
+        parser = new Ikea1(config, loadPriceParserWithCatalog());
+    }
+
+}
---
+++ parser-stores/src/main/java/com/openprice/parser/store/toysrus/ToysrusSelector.java
@@ -0,0 +1,41 @@
+package com.openprice.parser.store.toysrus;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.openprice.parser.ChainRegistry;
+import com.openprice.parser.StoreConfigImpl;
+import com.openprice.parser.api.ReceiptData;
+import com.openprice.parser.api.StoreParser;
+import com.openprice.parser.store.AbstractStoreParserSelector;
+import com.openprice.store.StoreMetadata;
+
+@Service
+public class ToysrusSelector extends AbstractStoreParserSelector {
+    private Toysrus1 parser;
+    private static final String CODE="Ikea";
+
+    @Inject
+    public ToysrusSelector(final ChainRegistry chainRegistry, final StoreMetadata metadata) {
+        super(chainRegistry, metadata);
+    }
+
+    @Override
+    public StoreParser selectParser(final ReceiptData receipt) {
+        // just one parser now. TODO based on receipt data to return parser
+        return parser;
+    }
+
+    @Override
+    protected String getParserBaseCode() {
+        return CODE;
+    }
+
+    @Override
+    protected void generateParser() {
+        StoreConfigImpl config = loadParserConfig(CODE+"1");
+        parser = new Toysrus1(config, loadPriceParserWithCatalog());
+    }
+
+}
