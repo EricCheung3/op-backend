fc552a09c81291d1710fb03d11a03b5635f3f890
hengshuai yao <hengshuai@gmail.com>
2016-02-26 11:48:14 -0700

    ItemPredictorImpl

--- parser-stores/src/test/java/com/openprice/parser/itempredictor/ItemPredictorImplTest.java
+++ parser-stores/src/test/java/com/openprice/parser/itempredictor/ItemPredictorImplTest.java
@@ -9,8 +9,20 @@ public class ItemPredictorImplTest {
     final private ItemPredictorImpl predictor = new ItemPredictorImpl();
 
     @Test
-    public void test1(){
+    public void normalInput(){
         final String line = "7040054391580 RIDER INSULATE $179.99 16 ";
         assertTrue(predictor.isItemLine(line));
+    }
+
+    @Test
+    public void donotSupportWithoutHeadingItemNumberNow(){
+        final String line = "RIDER INSULATE $179.99 16 ";
+        assertTrue(!predictor.isItemLine(line));
+    }
+
+    @Test
+    public void donotSupportWithoutTrailingPriceNow(){
+        final String line = "RIDER INSULATE $17999 16 ";
+        assertTrue(!predictor.isItemLine(line));
     }
 }
