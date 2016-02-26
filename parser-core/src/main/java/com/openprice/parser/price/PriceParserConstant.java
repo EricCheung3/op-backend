c991e03afc10cb0afe13e95c86be1370548acce7
hengshuai yao <hengshuai@gmail.com>
2016-02-25 23:38:02 -0700

    increasing min char length of item name

--- parser-core/src/main/java/com/openprice/parser/price/PriceParserConstant.java
+++ parser-core/src/main/java/com/openprice/parser/price/PriceParserConstant.java
@@ -5,7 +5,7 @@ public class PriceParserConstant {
     public static final int MIN_ITEM_NUMBER_LENGTH=4;
     public static final double MIN_ITEM_NUMBER_PERCENT=0.7;
 
-    public static final int MIN_ITEM_NAME_LETTERS=3;
+    public static final int MIN_ITEM_NAME_LETTERS = 2;
     public static final double MIN_ITEM_NAME_LETTERS_PERCENT=0.5;
 
 
