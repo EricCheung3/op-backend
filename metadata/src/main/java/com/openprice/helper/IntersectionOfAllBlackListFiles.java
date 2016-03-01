025c77768d2278860029ddae5d7157aa4907166b
hengshuai yao <hengshuai@gmail.com>
2016-02-29 21:45:37 -0700

    deleting black list files from stores because they are already merged to a single file'

--- metadata/src/main/java/com/openprice/helper/IntersectionOfAllBlackListFiles.java
+++ metadata/src/main/java/com/openprice/helper/IntersectionOfAllBlackListFiles.java
@@ -18,7 +18,7 @@ public class IntersectionOfAllBlackListFiles {
 
         final URL url = IntersectionOfAllBlackListFiles.class.getResource("/");
         File dir = new File((url.toURI()));
-        System.out.println("dir.listFiles().size="+dir.listFiles().toString());
+//        System.out.println("dir.listFiles().size="+dir.listFiles().toString());
         int countFiles = 0;
         final Set<String> all = new HashSet<String>();
         final Set<String> intersect = new HashSet<String>();
@@ -29,22 +29,26 @@ public class IntersectionOfAllBlackListFiles {
             try{
                 final List<String> orig = TextResourceUtils.loadStringArrayFromAbolutePath(Paths.get(fileName));
                 if(orig!=null){
-                    System.out.println("orig.size="+orig.size());
+//                    System.out.println("orig.size="+orig.size());
                     final List<String> list = ConvertTextToJson.removeCommentEmptyLines(orig);
-                    System.out.println("list.size="+list.size());
+//                    System.out.println("list.size="+list.size());
                     countFiles++;
                     all.addAll(list);
-                    if(countFiles==1)
+                    if(countFiles==1){
                         intersect.addAll(list);
-                    else if(list.size()>10)
+                    }else if(list.size() > 10){
                         intersect.retainAll(list);
+                    }else{
+                        System.out.println(""+file.toString());
+                    }
                 }
             }catch(Exception e){
+                System.out.println(""+file.toString());
                 continue;
             }
         }
 //        System.out.println("countFiles="+countFiles);
-        all.forEach(str -> System.out.println(str));
+//        all.forEach(str -> System.out.println(str));
 //        System.out.println("all.size="+all.size());
 //        System.out.println("intersect.size="+intersect.size());
 //        intersect.forEach(str -> System.out.println(str));
