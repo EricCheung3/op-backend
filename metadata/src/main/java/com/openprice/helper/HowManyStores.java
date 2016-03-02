9a62b05bdc378357ddf00883bade1b87c5b9473e
hengshuai yao <hengshuai@gmail.com>
2016-03-02 12:36:06 -0700

    comment

--- metadata/src/main/java/com/openprice/helper/HowManyStores.java
+++ metadata/src/main/java/com/openprice/helper/HowManyStores.java
@@ -3,6 +3,9 @@ package com.openprice.helper;
 import java.io.File;
 import java.net.URL;
 
+/**
+ * not very precise becaus it contains "com"
+ */
 public class HowManyStores {
     public static void main(String[] args) throws Exception{
 
@@ -10,8 +13,10 @@ public class HowManyStores {
         File dir = new File((url.toURI()));
         int countStores = 0;
         for (File file : dir.listFiles()) {
-            if(file.isDirectory())
+            if(file.isDirectory()){
+                System.out.println(file.getName());
                 countStores++;
+            }
         }
         System.out.println("We have "+ countStores + " parsers.");
     }
