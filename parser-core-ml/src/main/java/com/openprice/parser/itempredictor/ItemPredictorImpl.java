2d649d58daef94968a9f7eebf045d808aaef29bc
hengshuai yao <hengshuai@gmail.com>
2016-03-02 22:35:16 -0700

    pom changed

--- parser-core-ml/src/main/java/com/openprice/parser/itempredictor/ItemPredictorImpl.java
+++ parser-core-ml/src/main/java/com/openprice/parser/itempredictor/ItemPredictorImpl.java
@@ -8,7 +8,7 @@ public class ItemPredictorImpl implements ItemPredictor{
     @Override
     public boolean isItemLine(String line) {
         final SplittingFeatures features = generator.getFeatures(line);
-        return features.getNumCharsAtMiddle() >= PriceParserConstant.MIN_ITEM_NAME_LETTERS
+        return  features.getNumCharsAtMiddle() >= PriceParserConstant.MIN_ITEM_NAME_LETTERS
              && features.getNumHeadingDigits() >= PriceParserConstant.MIN_ITEM_NUMBER_LENGTH
              && features.getNumTrailingDigits() >= 2 //price digits has to be at least 2
              && features.isOneDollarSignAtTail()
