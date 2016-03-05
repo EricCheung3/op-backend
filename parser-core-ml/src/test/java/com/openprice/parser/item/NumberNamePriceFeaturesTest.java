ba0ed4d70f2d5887352dd81f8095494806fc7024
hengshuai yao <hengshuai@gmail.com>
2016-03-04 19:09:43 -0700

    no compilation errors

--- parser-core-ml/src/test/java/com/openprice/parser/item/NumberNamePriceFeaturesTest.java
+++ parser-core-ml/src/test/java/com/openprice/parser/item/NumberNamePriceFeaturesTest.java
@@ -7,16 +7,13 @@ import org.junit.Test;
 
 import com.openprice.common.StringCommon;
 import com.openprice.parser.ml.item.NumberNamePriceFeatures;
-import com.openprice.parser.ml.item.NumberNamePriceFeaturesGeneratorOld;
 
-public class ItemFeaturesTest {
-
-    private final NumberNamePriceFeaturesGeneratorOld generator = new NumberNamePriceFeaturesGeneratorOld();
+public class NumberNamePriceFeaturesTest {
 
     @Test
     public void normalInput(){
         final String str = "7040054391580 RIDER INSULATE $179.99 16 ";
-        NumberNamePriceSplit features = generator.getFeatures(str);
+        final NumberNamePriceFeatures features = NumberNamePriceFeatures.fromString(str);
         assertEquals("7040054391580".length(), features.getNumHeadingDigits());
         assertEquals(StringCommon.removeAllSpaces(" RIDER INSULATE").length(), features.getNumCharsAtMiddle());
         assertEquals("179.9916".length(), features.getNumTrailingDigits());//"." is counted
