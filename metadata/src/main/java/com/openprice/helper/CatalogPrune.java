4713785937516707f54e26d013e5abf537fdef0f
hengshuai yao <hengshuai@gmail.com>
2016-03-07 12:44:51 -0700

    safeway catalog data pruned

--- metadata/src/main/java/com/openprice/helper/CatalogPrune.java
+++ metadata/src/main/java/com/openprice/helper/CatalogPrune.java
@@ -1,5 +1,12 @@
 package com.openprice.helper;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.openprice.common.Levenshtein;
+import com.openprice.common.StringCommon;
 import com.openprice.store.ChainConfigFiles;
 import com.openprice.store.MetadataLoader;
 import com.openprice.store.data.ProductData;
@@ -10,16 +17,35 @@ import com.openprice.store.data.ProductData;
 
 public class CatalogPrune {
 
+    private static final double THRESHOLD = 0.75;
+
     public static void main(String[] args) throws Exception{
         final String chainCode = "safeway";
-        print(chainCode);
+        printSimilarNaturalName(chainCode);
     }
 
-    public static void print(final String chainCode){
+    //print products that have similar natural names
+    public static void printSimilarNaturalName(final String chainCode){
         final ProductData[] data = MetadataLoader.loadArrayFromJsonResource(ChainConfigFiles.getCatalog(chainCode), ProductData[].class);
-        System.out.println("data.length="+data.length);
-        for(int i=0; i< data.length; i++){
-            System.out.println(data[i].toString());
-        }
+        final List<ProductData> all = Arrays.asList(data);
+
+        final Set<ProductData> alreadyPrinted = new HashSet<>();
+        all
+        .stream()
+        .filter(p -> !alreadyPrinted.contains(p))
+        .forEach(p->{
+            all
+            .stream()
+            .filter(q -> !q.equals(p))
+            .forEach( q->{
+                double score = Levenshtein.compare(StringCommon.removeAllSpaces(p.getNaturalName().toLowerCase()),
+                        StringCommon.removeAllSpaces(q.getNaturalName().toLowerCase()));
+                if(score > THRESHOLD){
+                    System.out.println(p.getNaturalName()+", "+q.getNaturalName() +": score="+score);
+                    alreadyPrinted.add(p);
+                    alreadyPrinted.add(q);
+                }
+            });
+        });
     }
 }
